5fa62eb446687624a0667276e0ef6ab33762cead
saisahu <saisahu32@outlook.com>
2024-05-28 17:15:06 +0530

    Extra concepts practiced

---
+++ .ipynb_checkpoints/python_practice-checkpoint.py
@@ -0,0 +1,21 @@
+i = 0
+j = 0
+k = 0
+
+while i < 3:
+	# print("output from loop layer_1:", i)
+	
+	while j < 3:
+		print("output from loop layer_2:", j)
+	
+		if j == 1:
+			break
+
+		j = j + 1
+
+	# if i == 1:
+	# 	break
+
+	i = i + 1
+
+
